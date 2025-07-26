8dc0b6aee4d57ad7c6e104644108683f8c4bd18c
rajyadav63 <vector36razz@gmail.com>
2025-07-26 15:13:33 +0530

    home updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/60 antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Music Academy</h1>
+      <h1 className="text-3xl text-center">Music Academy</h1>
     </main>
   );
 }
