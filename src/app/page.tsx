6d1c568a9efd341883ca73ca8af167fcfb7177a3
rajyadav63 <vector36razz@gmail.com>
2025-07-27 16:39:53 +0530

    WhyUs section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import FeaturesCourses from "@/components/FeaturesCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import WhyUs from "@/components/WhyUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/60 antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturesCourses />
+      <WhyUs />
     </main>
   );
 }
